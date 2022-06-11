c045ae6d1346ccf1a47d33f8a108c711952a0f6d
dosha10 <ndungufrank01@gmail.com>
2022-06-11 13:30:07 +0300

    added more components/css modules & firebase rtb

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,23 @@
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return (
+    <div>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default MeetupList;
